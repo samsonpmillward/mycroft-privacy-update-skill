68c5055b437472435fde169e372d3dcd25bb6901
samson millward <up820264@myport.ac.uk>
2020-04-09 11:28:44 +0100

    0.9.2
    Added incrementing numbers to the news articles.

--- __init__.py
+++ __init__.py
@@ -12,12 +12,15 @@ class PrivacyUpdate(MycroftSkill):
         feed = feedparser.parse(url)
 
         for post in feed.entries:
+            i = 0
+            i = i + 1 
             date = "(%d/%02d/%02d)" % (post.published_parsed.tm_year,\
                 post.published_parsed.tm_mon, \
                 post.published_parsed.tm_mday)
             #print("post date: " + date)
             #print("post title: " + post.title)
             #print("post link: " + post.link))
+            self.speak_dialog('update.privacy', {'number': i})
             self.speak_dialog('update.privacy', {'news': post.title})
 
 
